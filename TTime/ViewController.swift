84a93c1177f348fc311648b554c5cd0a2445bd0b
auston salvana <austonsalvana@austons-macbook-pro.local>
2015-11-28 13:46:08 -0800

    Initial Commit

---
+++ TTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTime
+//
+//  Created by auston salvana on 2015-11-28.
+//  Copyright © 2015 ARJHack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
